33450769943e51d380909ed2ccb20936ca338214
Andy Jarrett <mail@andyjarrett.com>
2010-11-22 07:43:57 +0000

    Getting ready to move to a new git repo

--- distro_all.sh
+++ distro_all.sh
@@ -8,12 +8,4 @@
 ./distro_cf5.sh
 ./distro_bd70.sh
 ./distro_bd62.sh
-
-
-zip -r /Users/andyjarrett/Desktop/Railo.tmbundle.zip /Users/andyjarrett/Desktop/Railo.tmbundle	
-zip -r /Users/andyjarrett/Desktop/ColdFusion-5.tmbundle.zip /Users/andyjarrett/Desktop/ColdFusion-5.tmbundle
-zip -r /Users/andyjarrett/Desktop/ColdFusion-6.1.tmbundle.zip /Users/andyjarrett/Desktop/ColdFusion-6.1.tmbundle
-zip -r /Users/andyjarrett/Desktop/ColdFusion-7.tmbundle.zip /Users/andyjarrett/Desktop/ColdFusion-7.tmbundle
-zip -r /Users/andyjarrett/Desktop/ColdFusion-8.tmbundle.zip /Users/andyjarrett/Desktop/ColdFusion-8.tmbundle
-zip -r /Users/andyjarrett/Desktop/BlueDragon-6.2.tmbundle.zip /Users/andyjarrett/Desktop/BlueDragon-6.2.tmbundle
-zip -r /Users/andyjarrett/Desktop/BlueDragon-7.0.tmbundle.zip /Users/andyjarrett/Desktop/BlueDragon-7.0.tmbundle		
+	
